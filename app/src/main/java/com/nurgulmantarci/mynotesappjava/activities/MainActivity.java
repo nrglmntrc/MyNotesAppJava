cb6b9340c8733513795df270a1a55060ddc9b763
Nurgül Mantarcı <nurgulmantarci28@gmail.com>
2021-06-11 12:25:59 +0300

    intro Activity done

--- app/src/main/java/com/nurgulmantarci/mynotesappjava/activities/MainActivity.java
+++ app/src/main/java/com/nurgulmantarci/mynotesappjava/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.nurgulmantarci.mynotesappjava;
+package com.nurgulmantarci.mynotesappjava.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.nurgulmantarci.mynotesappjava.R;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
