9e406c04f3258518b67e4da08a33e76ffff9a0ce
Nurgül Mantarcı <nurgulmantarci28@gmail.com>
2021-06-11 11:41:25 +0300

    initial commit

---
+++ app/src/main/java/com/nurgulmantarci/mynotesappjava/MainActivity.java
@@ -0,0 +1,15 @@
+package com.nurgulmantarci.mynotesappjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
