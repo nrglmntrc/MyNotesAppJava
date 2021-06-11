1bca781caed4abe950695f227b1840f9fa80b5bf
Nurgül Mantarcı <nurgulmantarci28@gmail.com>
2021-06-11 12:42:32 +0300

    Splash Activity done

--- app/src/main/java/com/nurgulmantarci/mynotesappjava/activities/SplashActivity.java
+++ app/src/main/java/com/nurgulmantarci/mynotesappjava/activities/SplashActivity.java
@@ -2,9 +2,11 @@ package com.nurgulmantarci.mynotesappjava.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.nurgulmantarci.mynotesappjava.R;
+import com.nurgulmantarci.mynotesappjava.intro.WelcomeActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -12,5 +14,25 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        splash();
+    }
+
+    public void splash() {
+        Thread timerTread = new Thread() {
+            public void run() {
+                try {
+                    sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(getApplicationContext(), WelcomeActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timerTread.start();
     }
 }
